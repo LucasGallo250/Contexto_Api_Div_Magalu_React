183df26524e2ae66fc54be4ab955af5a4af5c22c
Lucas Gallo <lucasgallo5046@gmail.com>
2023-03-21 20:17:53 -0300

    Criação para logar, deslogar e estilização no CSS

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import React from 'react'
 import Login from './components/login';
 import Profile from './components/profile';
 import Footer from './components/footer';
-import { useAuth } from './providers/auth'
+import { useAuth } from './providers/auth';
+//CSS Modules
+import styles from './style.module.css';
+
 
 function App() {
   const { user } = useAuth()
   console.log(user)
 
+  const [ isLogin, setIsLogin ] = React.useState(false); //false iniciando como boolean
+
+  const handleDialogLogin = () => {
+    setIsLogin(!isLogin)
+  }
+
   return (
-    <>
+    <div className={styles.container}>
       <h1>Olá GamaAcademy</h1>
       <Profile />
-      <Login />
+      { !isLogin && (
+        <div>
+          <button onClick={handleDialogLogin}>Deseja logar?</button>
+        </div>
+      )}
+      
+      { isLogin && (
+        <>
+          <div className={styles.box} >
+            <button onClick={handleDialogLogin}>Fechar</button>
+            <Login />
+          </div>
+        </>
+      )}
       <Footer />
-    </>
+    </div>
   );
 }
 
